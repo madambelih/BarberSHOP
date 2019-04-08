49ddd3cc3a2e2caa568a2c36023c5557027edabc
Aliya <madambelih@gmail.com>
2019-04-08 12:30:18 +0300

    Create database on app unit

---
+++ app.rb
@@ -0,0 +1,58 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+configure do
+	@db=SQlite::Database.new 'barbershop.db'
+	@db.execute 'CREATE TABLE IF NOT EXISTS
+	"Users"
+	(
+		"id"INTEGER PRIMARY KEY AUTOINCREMENT,
+		"username" TEXT,
+		"phone" TEXT,
+		"datestamp" TEXT,
+		"barber" TEXT,
+		"color" TEXT,
+		)'
+end
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	# хеш
+	hh = { 	:username => 'Введите имя',
+			:phone => 'Введите телефон',
+			:datetime => 'Введите дату и время' }
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ''
+		return erb :visit
+	end
+
+	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+
+end
+
+get '/contact' do
+	erb :contact
+end
+
